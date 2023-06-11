065bd5da72dede244cc81bfcdf3b6587d46002a2
Mircea Andone <mircea.andone@outlook.com>
2023-06-11 19:10:59 +0300

    :sparkles: added echo

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	log.Println("Welcome from the backend server")
+	app := echo.New()
+	app.Use(middleware.Logger())
+	app.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	app.Logger.Fatal(app.Start(":1323"))
 }
